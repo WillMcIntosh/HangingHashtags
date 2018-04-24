9d7fe8445f2addd4061ec43f301e637d70d4e14f
WillMcIntosh <mcintoshwilliamh@gmail.com>
2018-04-24 15:00:06 -0400

    changed javascript to target folder tabs or comment tabs correctly

--- scripts.js
+++ scripts.js
@@ -1,35 +1,54 @@
-window.addEventListener("load", function() {
-  // store all folder tabs as array
-  const folderTabs = document.getElementById("folder-tabs").querySelectorAll("ul.nav-tabs > li");
+// store all folder and comment tabs as two arrays
+const folderTabs = document.getElementById("folder-tabs").querySelectorAll("ul.nav-tabs > li");
+const commentTabs = document.getElementById("comment-tabs").querySelectorAll("ul.nav-tabs > li");
 
-  function tabClicked(event) {
-    event.preventDefault(); // prevent URL from changing
+// store contents in two arrays
+const folderContents = document.getElementById("folder-content").querySelectorAll(".tab-pane");
+const commentContents = document.getElementById("comment-content").querySelectorAll(".tab-pane");
 
-    // remove active class from all tabs
-    for (tab of folderTabs) {
+// event listeners for tab clicks
+for (tab of folderTabs) {
+  tab.addEventListener("click", tabClicked);
+}
+
+for (tab of commentTabs) {
+  tab.addEventListener("click", tabClicked);
+}
+
+function tabClicked(event) {
+  event.preventDefault(); // prevent URL from changing
+  // define tab clicked
+  const clickedTab = event.currentTarget;
+  // find parent of tab clicked to find if classList contains comments or folders
+
+  const parent = clickedTab.parentElement;
+
+  // remove active class from appropriate tabs and content sections
+  if (parent.classList.contains("comment-section")) {
+    for (tab of commentTabs) {
       tab.classList.remove("active");
     }
 
-    // give clicked tab the active class
-    const clickedTab = event.currentTarget;
-    clickedTab.classList.add("active");
-
-    // store all contents in array
-    const folderContents = document.getElementById("folder-content").querySelectorAll(".tab-pane");
-
-    // remove active class from all contents
+    for (content of commentContents) {
+      content.classList.remove("active");
+    }
+  }
+  else {
+    for (tab of folderTabs) {
+      tab.classList.remove("active");
+    }
     for (content of folderContents) {
       content.classList.remove("active");
     }
-
-    // add active class to folder corresponding to tab
-    const activeFolderId = clickedTab.querySelector("a").getAttribute("href");
-    const activeFolder = document.querySelector(activeFolderId);
-
-    activeFolder.classList.add("active");
   }
 
-  for (tab of folderTabs) {
-    tab.addEventListener("click", tabClicked);
-  }
-});
+  // give clicked tab the active class
+  clickedTab.classList.add("active");
+
+  // add active class to content corresponding to tab clicked
+  const activeContentId = clickedTab.querySelector("a").getAttribute("href");
+  const activeContent = document.querySelector(activeContentId);
+
+  activeContent.classList.add("active");
+}
+
