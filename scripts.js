6ce6dd3d2edcb6168a6cfd0f2c6ece57277d78d8
WillMcIntosh <mcintoshwilliamh@gmail.com>
2018-04-24 14:23:28 -0400

    tab functionality

---
+++ scripts.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", function() {
+  // store all folder tabs as array
+  const folderTabs = document.querySelectorAll("ul.nav-tabs > li");
+
+  function tabClicked(event) {
+    event.preventDefault(); // prevent URL from changing
+
+    // remove active class from all tabs
+    for (tab of folderTabs) {
+      tab.classList.remove("active");
+    }
+
+    // give clicked tab the active class
+    const clickedTab = event.currentTarget;
+    clickedTab.classList.add("active");
+
+    // store all contents in array
+    const folderContents = document.querySelectorAll(".tab-pane");
+
+    // remove active class from all contents
+    for (content of folderContents) {
+      content.classList.remove("active");
+    }
+
+    // add active class to folder corresponding to tab
+    const activeFolderId = clickedTab.querySelector("a").getAttribute("href");
+    const activeFolder = document.querySelector(activeFolderId);
+
+    activeFolder.classList.add("active");
+  }
+
+  for (tab of folderTabs) {
+    tab.addEventListener("click", tabClicked);
+  }
+});
